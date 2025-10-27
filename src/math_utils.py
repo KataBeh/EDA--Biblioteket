fd64a9774101ba27091e05f7053d0723e5ecab22
Katarina <kaisaf22@gmail.com>
2025-10-27 17:24:28 +0100

    Add io_utils with REQUIRED validation and numeric coercion

---
+++ src/math_utils.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    return a + b
+
+def multipy (a, b):
+    return a * b
+
+#print(add(3, 7))
+#print(multipy(2, 10))
+
+print("Namnet på filmen är:", __name__)
+
+if __name__ == "__main__":
+    print(add(3, 7))
+    print(multipy(2, 10))
+    
